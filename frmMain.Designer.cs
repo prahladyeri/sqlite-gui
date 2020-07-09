c7ab045bca281962802d65adafe11d064db15169
Prahlad Yeri <prahladyeri@yahoo.com>
2020-07-09 22:24:13 +0530

    chore: initial commit

---
+++ frmMain.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace sqlite_gui
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvMain = new System.Windows.Forms.DataGridView();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.lblPath = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMain)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvMain
+            // 
+            this.dgvMain.AllowUserToOrderColumns = true;
+            this.dgvMain.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.dgvMain.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvMain.Location = new System.Drawing.Point(12, 38);
+            this.dgvMain.Name = "dgvMain";
+            this.dgvMain.Size = new System.Drawing.Size(560, 264);
+            this.dgvMain.TabIndex = 0;
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(13, 13);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(75, 23);
+            this.btnBrowse.TabIndex = 1;
+            this.btnBrowse.Text = "Browse";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // lblPath
+            // 
+            this.lblPath.AutoSize = true;
+            this.lblPath.Location = new System.Drawing.Point(95, 19);
+            this.lblPath.Name = "lblPath";
+            this.lblPath.Size = new System.Drawing.Size(38, 13);
+            this.lblPath.TabIndex = 2;
+            this.lblPath.Text = "Ready";
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 314);
+            this.Controls.Add(this.lblPath);
+            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(this.dgvMain);
+            this.MaximizeBox = false;
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "sqlite-gui";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMain)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvMain;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.Label lblPath;
+    }
+}
+
---
+++ frmMain.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SQLite;
+using System.Collections;
+
+namespace sqlite_gui
+{
+    public partial class frmMain : Form
+    {
+        private SQLiteConnection conn = null;
+
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void openConn(string filename) {
+            string connstr = @"Data Source=" + filename + @";Version=3;New=True;Compress=True";
+            conn = new SQLiteConnection(connstr);
+            SQLiteDataAdapter da = new SQLiteDataAdapter("", conn);
+            DataSet ds = new DataSet();
+            da.SelectCommand.CommandText = "SELECT name FROM sqlite_master WHERE type = 'table' ORDER BY 1;";
+            da.Fill(ds);
+
+            ArrayList al = new ArrayList();
+            foreach (DataRow row in ds.Tables[0].Rows) {
+                //MessageBox.Show("TABLE: " + row[0]);
+                al.Add(row[0]);
+            }
+            frmChooseTable chooseTable = new frmChooseTable(al);
+            chooseTable.ShowDialog();
+            if (chooseTable.tableName != "") {
+                MessageBox.Show(chooseTable.tableName + " chosen");
+            }
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            //MessageBox.Show("Hello, World!");
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "SQLITE Database Files|*.sqlite3;*.db;";
+            DialogResult dr = ofd.ShowDialog();
+            if (dr == System.Windows.Forms.DialogResult.OK) {
+                var filename = ofd.FileName;
+                //MessageBox.Show(fn);
+                openConn(filename);
+            }
+        }
+    }
+}
